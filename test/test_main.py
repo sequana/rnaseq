e4aba994449cbb84c3f0deb5ca65d6a9e4326c47
Thomas Cokelaer <cokelaer@gmail.com>
2019-12-16 22:00:16 +0100

    update tests

--- test/test_main.py
+++ test/test_main.py
@@ -3,21 +3,28 @@ import os
 import tempfile
 import subprocess
 import sys
+from sequana.pipelines_common import get_pipeline_location as getpath
 
+sharedir = getpath("rnaseq")
 
-sequana_path = easydev.get_package_location('sequana_rnaseq')
-sharedir = os.sep.join([sequana_path , "sequana", 'data'])
+#sequana_path = easydev.get_package_location('sequana_rnaseq')
+#sharedir = os.sep.join([sequana_path , "sequana", 'data'])
 
 
 def test_standalone_subprocess():
     directory = tempfile.TemporaryDirectory()
-    #cmd = """sequana_pipelines_rnaseq --fastq-directory {} --output-directory {}""".format(
-    #    sharedir, directory.name)
-    #subprocess.call(cmd.split())
+    cmd = """sequana_pipelines_rnaseq --input-directory {} --working-directory {}""".format(
+        sharedir, directory.name)
+    subprocess.call(cmd.split())
 
 
-def test_standalone_script():
+def _test_standalone_script():
     directory = tempfile.TemporaryDirectory()
     import sequana_pipelines.rnaseq.main as m
-    #sys.argv = ["test", "--fastq-directory", sharedir, "--output-directory", directory.name]
-    #m.main()
+    sys.argv = ["test", "--fastq-directory", sharedir, "--output-directory", directory.name]
+    m.main()
+
+def test_version():
+    cmd = "sequana_pipelines_rnaseq --version"
+    subprocess.call(cmd.split())
+
