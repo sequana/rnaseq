4c61b71a89b9722806d0ce0d610d66718f76d333
Thomas Cokelaer <cokelaer@gmail.com>
2020-01-28 13:31:42 +0100

    add new sanity checks and options

--- sequana_pipelines/rnaseq/main.py
+++ sequana_pipelines/rnaseq/main.py
@@ -36,8 +36,7 @@ class Options(argparse.ArgumentParser):
 
             For instance, if you work on  Saccer3, type::
 
-                sequana_pipelines_rnaseq --input-directory \
-                    --genome-directory GENOMESPATH/Saccer3
+                sequana_pipelines_rnaseq --input-directory --genome-directory GENOMESPATH/Saccer3
 
             The pipeline will search for the files Saccer3.fa and Saccer3.gff in
             the directory GENOMESPATH/Saccer3.
@@ -68,21 +67,24 @@ class Options(argparse.ArgumentParser):
         pipeline_group.add_argument("--aligner", dest="aligner", required=True,
             choices=['bowtie2', 'bowtie1', 'star'],
             help= "a mapper in bowtie, bowtie2, star")
-        pipeline_group.add_argument("--do-indexing", dest="do_indexing", 
-            action="store_true", help="""If bowtie/star indexing file are 
-                not computed, you will need to compute them by setting 
+        pipeline_group.add_argument("--do-indexing", dest="do_indexing",
+            action="store_true", help="""If bowtie/star indexing file are
+                not computed, you will need to compute them by setting
                 this option """)
         pipeline_group.add_argument("--force-indexing", action="store_true",
             default=False,
-            help="""If indexing files exists already, but you wish to 
+            help="""If indexing files exists already, but you wish to
                 create them again, use this option""")
+        pipeline_group.add_argument("--rRNA-feature",
+            default="rRNA",
+            help="""Feature name corresponding to the rRNA to be identified for QCs""")
 
         # cutadapt related
         so = CutadaptOptions()
         so.add_options(self)
 
         # fastq_screen
-        pipeline_group = self.add_argument_group("pipeline_fastq_screen")
+        pipeline_group = self.add_argument_group("section_fastq_screen")
         pipeline_group.add_argument("--do-fastq-screen", action="store_true",
             default=False,
             help="run fastq_screen ")
@@ -93,14 +95,19 @@ documentation for details. In a nutsheel, add a line for each genome you want to
 search for in your input data. Each line is 'DATABASE name path BOWTIE2'. The
 path includes the path to the genome + its prefix name.  """)
 
+        pipeline_group = self.add_argument_group("section_feature_counts")
+        pipeline_group.add_argument("--feature-counts-options",
+            default="-t gene -g ID -s 1",
+            help="""options for feature counts. -t should be followed by a valid
+feature type, -g by a valid attribute name and -s  by the value 0 (unstranded),
+1( stranded) or 2 (reversely stranded)""")
 
         pipeline_group = self.add_argument_group("pipeline_others")
         pipeline_group.add_argument('--do-igvtools', action="store_true")
         pipeline_group.add_argument('--do-bam-coverage', action="store_true")
         pipeline_group.add_argument('--do-mark-duplicates', action="store_true")
-        pipeline_group.add_argument('--do-rnaseq-qc', action="store_true")
-
-
+        pipeline_group.add_argument('--do-rnaseq-qc', action="store_true",
+            help="not yet implemented")
 
 
 def main(args=None):
@@ -127,6 +134,7 @@ def main(args=None):
     cfg.general.aligner = options.aligner
     cfg.general.indexing = options.do_indexing
     cfg.general.force_indexing = options.force_indexing
+    cfg.general.rRNA_feature = options.rRNA_feature
 
     # --------------------------------------------------------- cutadapt
     cfg.cutadapt.do = not options.skip_cutadapt
@@ -138,6 +146,10 @@ def main(args=None):
     cfg.input_pattern = options.input_pattern
     cfg.input_readtag = options.input_readtag
 
+    # ----------------------------------------------------- feature counts
+    cfg.feature_counts.options = options.feature_counts_options
+
+    # ------------------------------------------------------ optional
     cfg.igvtools.do = options.do_igvtools
     cfg.coverage.do = options.do_bam_coverage
     cfg.mark_duplicates.do = options.do_mark_duplicates
@@ -156,6 +168,41 @@ def main(args=None):
         import sequana_pipelines.rnaseq
         shutil.copy(os.path.join(sequana_pipelines.rnaseq.__path__[0] ,
             "fastq_screen.conf"), manager.workdir)
+
+
+    # SANITY CHECKS
+    # -------------------------------------- do we find rRNA feature in the GFF ?
+    logger.info("checking your input GFF file and rRNA feature if provided")
+    from sequana.gff3 import GFF3
+    genome_directory = os.path.abspath(cfg["general"]["genome_directory"])
+    genome_name = genome_directory.rsplit("/", 1)[1]
+    prefix_name = genome_directory + "/" + genome_name
+    gff_file = prefix_name + ".gff"
+    gff = GFF3(gff_file)
+    valid_types = gff.get_types()
+
+    if cfg['general']["rRNA_feature"] and \
+        cfg['general']["rRNA_feature"] not in valid_types:
+
+        logger.error("rRNA feature not found in the input GFF ({})".format(gff_file) +
+            " This is probably an error. Please check the GFF content and /or"
+            " change the feature name with --rRNA-feature based on the content" 
+            " of your GFF. Valid features are: {}".format(valid_types))
+        sys.exit()
+
+    valid_types = gff.get_types()
+    fc_options = cfg["feature_counts"]["options"]
+    index = fc_options.split().index('-t')
+    fc_type = fc_options.split()[index+1]
+
+
+    logger.info("checking your input GFF file and feature counts options")
+    if fc_type not in valid_types:
+        logger.error("Invalid type ({}) in feature_count section of config.yaml (options {}). Valid types found in your GFF file are: {}".format(fc_type, fc_options, valid_types))
+
+        sys.exit()
+
+
 
 
     # finalise the command and save it; copy the snakemake. update the config
