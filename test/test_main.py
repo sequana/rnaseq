651b2c162c4b1d1aa85dc41235cd122726a45278
Thomas Cokelaer <cokelaer@gmail.com>
2021-10-06 14:33:20 +0200

    update test

--- test/test_main.py
+++ test/test_main.py
@@ -3,13 +3,12 @@ import os
 import tempfile
 import subprocess
 import sys
-from sequana.pipelines_common import get_pipeline_location as getpath
 
-sharedir = getpath("rnaseq")
-saccer3 = getpath("rnaseq") + "/Saccer3/"
+from . import test_dir
 
-#sequana_path = easydev.get_package_location('sequana_rnaseq')
-#sharedir = os.sep.join([sequana_path , "sequana", 'data'])
+
+sharedir = f"{test_dir}/data"
+saccer3 = f"{test_dir}/data/Saccer3/"
 
 
 def test_standalone_subprocess():
@@ -28,8 +27,54 @@ def test_standalone_script():
         "--rRNA-feature", "rRNA_gene"]   # ideally should be rRNA but current
     m.main()
 
+def test_standalone_script_contaminant():
+    directory = tempfile.TemporaryDirectory()
+    import sequana_pipelines.rnaseq.main as m
+    sys.argv = ["test", "--input-directory", sharedir, "--genome-directory",
+        saccer3, "--force", "--aligner", "bowtie2", 
+             "--feature-counts-feature-type", 'gene,tRNA', 
+            "--contaminant-file", "test.fa",
+        "--rRNA-feature", "rRNA_gene"]   # ideally should be rRNA but current
+    m.main()
 
 def test_version():
     cmd = "sequana_pipelines_rnaseq --version"
     subprocess.call(cmd.split())
 
+
+def test_standalone_script_wrong_feature():
+    directory = tempfile.TemporaryDirectory()
+    import sequana_pipelines.rnaseq.main as m
+    sys.argv = ["test", "--input-directory", sharedir, "--genome-directory",
+        saccer3, "--force", "--aligner", "bowtie2", 
+             "--feature-counts-feature-type", 'dummy', 
+        "--rRNA-feature", "rRNA_gene"]   # ideally should be rRNA but current
+    try:
+        m.main()
+        assert False
+    except:
+        assert True
+
+def test_standalone_script_wrong_reference():
+    directory = tempfile.TemporaryDirectory()
+    import sequana_pipelines.rnaseq.main as m
+    sys.argv = ["test", "--input-directory", sharedir, "--genome-directory",
+        "dummy", "--force", "--aligner", "bowtie2", 
+        "--rRNA-feature", "rRNA_gene"]   # ideally should be rRNA but current
+    try:
+        m.main()
+        assert False
+    except:
+        assert True
+
+def test_standalone_script_wrong_triming():
+    directory = tempfile.TemporaryDirectory()
+    import sequana_pipelines.rnaseq.main as m
+    sys.argv = ["test", "--input-directory", sharedir, "--genome-directory",
+        saccer3, "--force", "--aligner", "bowtie2", "--software-choice", "dummy",
+        "--rRNA-feature", "rRNA_gene"]   # ideally should be rRNA but current
+    try:
+        m.main()
+        assert False
+    except SystemExit:
+        assert True
