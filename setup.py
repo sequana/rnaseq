3d84d00e5ad7311d0fbcadda4624c829354c75cb
Thomas Cokelaer <cokelaer@gmail.com>
2023-07-06 23:16:36 +0200

    Fix options not used in star_mapping

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import subprocess
 
 _MAJOR = 0
 _MINOR = 17
-_MICRO = 1
+_MICRO = 2
 version = "%d.%d.%d" % (_MAJOR, _MINOR, _MICRO)
 release = "%d.%d" % (_MAJOR, _MINOR)
 
@@ -25,9 +25,9 @@ metainfo = {
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Topic :: Scientific/Engineering :: Information Analysis",
