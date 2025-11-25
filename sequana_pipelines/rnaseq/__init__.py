a0bdce860ccd278d7cbe3b8311035edc68972f72
Thomas Cokelaer <cokelaer@gmail.com>
2025-11-25 11:15:08 +0100

    remove deprecated pkg_resources

--- sequana_pipelines/rnaseq/__init__.py
+++ sequana_pipelines/rnaseq/__init__.py
@@ -1,6 +1,12 @@
-import pkg_resources
-try:
-    version = pkg_resources.require("sequana_rnaseq")[0].version
-except:
-    version = ">=0.8.0"
+import importlib.metadata as metadata
 
+
+def get_package_version(package_name):
+    try:
+        version = metadata.version(package_name)
+        return version
+    except metadata.PackageNotFoundError:
+        return f"{package_name} not found"
+
+
+version = get_package_version("sequana-lora")
